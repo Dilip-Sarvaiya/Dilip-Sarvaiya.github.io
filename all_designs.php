ed07b6a41b07673b886c0af5365627e018c0e49f
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-12-06 17:33:17 +0530

    Personal Web

---
+++ all_designs.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>My Designs</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="assets/img/pic.ico" type="image/x-icon">
+   <link href="assets/img/pic.ico"  type="image/x-icon" rel="shortcut icon">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container" style="margin-top:50px;">
+   <a  target="_blank" href="My_Designs/Image_Gallery_with_Lightbox/" ><div class="well">Image Gallery with Lightbox CSS</div>
+  <a  target="_blank" href="My_Designs/Social_media_css/" ><div class="well">Social media icons hover effect using css</div></a>
+  <a  target="_blank" href="My_Designs/Leaves_CSS_with_Name/" ><div class="well">Leaves Animation with name using HTML and CSS</div></a>
+
+  <a  target="_blank" href="My_Designs/3D_Image_Gallery/" ><div class="well">3D Image Gallery</div></a>
+  <a  target="_blank" href="My_Designs/Text_Animation/" ><div class="well">Text_Animation</div></a>
+  <a  target="_blank" href="My_Designs/Calculator/" ><div class="well">Calculator using Javascript</div></a>
+</div>
+</body>
+</html>  
